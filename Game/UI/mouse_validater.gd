34ef0b92d1ab9a6028cfd6b0272e362f90bbeac1
Kreuterkeule <moritz.siefke@protonmail.com>
2023-11-29 18:45:27 +0100

    git setup should be complete

--- Game/UI/mouse_validater.gd
+++ Game/UI/mouse_validater.gd
@@ -8,5 +8,3 @@ func _process(_delta):
 	var mouse_grid_coords :Vector2 = $ValidatingTiles.local_to_map(get_global_mouse_position());
 	var is_valid_field = !Game.map.is_field_occupied(mouse_grid_coords); # the maps have the same coords
 	$ValidatingTiles.set_cell(0, mouse_grid_coords, 0, GREEN_FIELD_COORDS if is_valid_field else RED_FIELD_COORDS, 0);
-
-
